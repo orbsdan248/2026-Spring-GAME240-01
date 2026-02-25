1dc02a0a20b37518b7406451bd512e8cde0dc950
orbsdan <giovanola@chapman.edu>
2026-02-24 16:59:05 -0800

    Not sure why there are so many files. Copied some CS files

---
+++ homework/february_17/submission/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+Console.WriteLine("Hello, What is your name?");
+
+var name = Console.ReadLine();
+Console.Write("Hello There ");
+Console.WriteLine(name);
+
+Console.WriteLine("What is your age?");
+var age = Console.ReadLine();
+
+Console.WriteLine("is it your birthday?");
+
+var isbirthday  = Console.ReadLine();
+if (isbirthday == "yes")
+{
+    Console.WriteLine("Happy Birthday!");
+}
+else if  (isbirthday == "no")
+{
+    Console.WriteLine("Oh. Sorry it's not your birthday.");
+}
+else
+{
+    Console.WriteLine("What do you mean? How did you forget your birthday?");
+}
+{}
